1576e9f5ff5befc92c92bb50ad74364c9bdb7a3b
tnraddatz <tnraddatz@gmail.com>
2020-12-29 13:25:33 -0500

    Added Auth

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
+import LoginButton from "./LoginButton"
+import LogoutButton from "./LogoutButton"
+import Profile from "./Profile"
 
 function Home() {
   return (
     <div> 
         Home Page
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <Auth0Provider
+          domain="dev-raddatzt.us.auth0.com"
+          clientId="JNx1LjocuNBgoHHXFhEx0y7eerQBw685"
+          redirectUri={window.location.origin}
+        >
+          <App />
+        </Auth0Provider>
       </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
