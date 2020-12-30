daa36537dddd3d1c54686abb7d5e5cebca331aec
tnraddatz <tnraddatz@gmail.com>
2020-12-29 20:45:53 -0500

    Added OpenId

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ ReactDOM.render(
           domain="dev-raddatzt.us.auth0.com"
           clientId="JNx1LjocuNBgoHHXFhEx0y7eerQBw685"
           redirectUri={window.location.origin}
+          audience="https://internalwiki/api"
+          scope="read:current_user update:current_user_metadata openid"
         >
           <App />
         </Auth0Provider>
